f6bcd0fa3bcdc7bf5f26167b59a836cb7eac6b53
thxtdy <thjjang0409@gmail.com>
2024-10-07 14:14:59 +0900

    first commit

---
+++ src/main/java/com/tenco/blog_v1/board/BoardController.java
@@ -0,0 +1,78 @@
+package com.tenco.blog_v1.board;
+
+import com.tenco.blog_v1.BoardNativeRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+   // 의존성 주입
+    private final BoardNativeRepository boardNativeRepository;
+
+    @GetMapping("/")
+    public  String index(Model model){
+        List<Board> boardList = boardNativeRepository.findAll();
+        model.addAttribute("boardList", boardList);
+        log.warn("여기까지 실행됨");
+        return  "index";
+    }
+
+    // 주소설계 : http://localhost:8080/board/save-form
+    //게시글 작성 화면 연결
+    @GetMapping("/board/save-form")
+    public String saveForm(){
+        return "board/save-form";
+    }
+
+    @PostMapping("/board/save")
+    public  String save(@RequestParam(name = "title") String title ,@RequestParam(name = "content")String content ){
+        log.warn("save 실행 : 제목={} , 내용={}" , title , content);
+        boardNativeRepository.save(title,content);
+    return  "redirect:/"; //게시글 목록으로 이동
+    }
+
+    // 특정 게시글 요청화면
+    // 주소설계 : http://localhost:8080/board/10
+    @GetMapping("board/{id}")
+    public String detail(@PathVariable(name="id")Integer id , HttpServletRequest request) {
+
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board" , board);
+
+        return "board/detail";
+    }
+    // 주소설계 http://localhost:8080/board/10/delete ( form 태그 활용이기 때문에 delete 선언 )
+    // form 태그에서는 GET, POST 방식만 지원하기 때문.
+    @PostMapping("/board/{id}/delete")
+    public String delete(@PathVariable(name = "id") Integer id) {
+        boardNativeRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    // 게시글 수정 화면 요청
+    // 주소설계 : http://localhost:8080/board/{id}/update-form
+    @GetMapping("board/{id}/update-form")
+    public  String updateForm(@PathVariable(name = "id") Integer id , HttpServletRequest request){
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board", board);
+        return  "board/update-form"; // src/main/resources/templates/update-form.mustache
+    }
+
+   // 게시글 수정 기능
+   // board/{id}/update
+   @PostMapping("/board/{id}/update")
+   public String update(@PathVariable(name = "id") Integer id, @RequestParam(name = "title") String title, @RequestParam(name = "content") String content) {
+       boardNativeRepository.updateById(id, title, content);
+       return "redirect:/board/" + id;
+   }
+}
